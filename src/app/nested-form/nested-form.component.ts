50d3776a15ee1e68d8e106308e71e919a65826e9
8800803 <sardaryawarabbas@gmail.com>
2022-11-04 10:18:36 +0500

    working fine

--- src/app/nested-form/nested-form.component.ts
+++ src/app/nested-form/nested-form.component.ts
@@ -5,6 +5,7 @@ import {
   UntypedFormControl,
   UntypedFormGroup,
 } from '@angular/forms';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-nested-form',
@@ -12,12 +13,52 @@ import {
   styleUrls: ['./nested-form.component.scss'],
 })
 export class NestedFormComponent implements OnInit {
+  isHidden$!: Observable<boolean>;
   constructor(private _fb: FormBuilder) {}
   hidden: boolean = true;
   test: any;
   value: any;
   ngOnInit(): void {
     this.getHiddenFields();
+    this.group.valueChanges.subscribe((selectedValue: any) => {
+      const keyValue = this.getDirtyValues(this.group);
+
+      for (var key in keyValue) {
+        for (var keyInner in keyValue[key]) {
+          let temp: any = this.element.fields.values.filter(
+            (x: any) => keyInner == x.dependsOn
+          );
+          let dependStateArray: any = temp[0].dependState;
+
+          let original: any = keyValue[key];
+          dependStateArray.forEach((el: any) => {
+            if (original[keyInner] === el) {
+              temp[0].hidden = false;
+              this.hidden = false;
+            }
+          });
+          // if(original[keyInner] == temp)
+        }
+      }
+
+      //this.element.fields.values.filter((x:any) => value.includes(x.GroupId));
+    });
+  }
+
+  getDirtyValues(form: any) {
+    let dirtyValues: any = {};
+
+    Object.keys(form.controls).forEach((key) => {
+      const currentControl = form.controls[key];
+
+      if (currentControl.dirty) {
+        if (currentControl.controls)
+          dirtyValues[key] = this.getDirtyValues(currentControl);
+        else dirtyValues[key] = currentControl.value;
+      }
+    });
+
+    return dirtyValues;
   }
 
   @Input() element: any;
@@ -29,7 +70,6 @@ export class NestedFormComponent implements OnInit {
   }
 
   somethingChanged(event: any) {
-    console.log(event);
     if (
       event.target.value == this.value[0] ||
       event.target.value == this.value[1]
