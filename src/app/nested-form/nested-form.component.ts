9d4a1c89392ef533d31364f41ddd90ab44147165
parvinkh <you@example.com>
2022-11-04 09:55:09 +0330

    Fix: dependOn

--- src/app/nested-form/nested-form.component.ts
+++ src/app/nested-form/nested-form.component.ts
@@ -1,68 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, UntypedFormGroup } from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { UntypedFormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-nested-form',
   templateUrl: './nested-form.component.html',
   styleUrls: ['./nested-form.component.scss'],
 })
-export class NestedFormComponent implements OnInit {
-  constructor(private _fb: FormBuilder) {}
-  hidden: boolean = true;
-  test: any;
-  value: any;
-  ngOnInit(): void {
-    this.group.valueChanges.subscribe((selectedValue: any) => {
-      const keyValue = this.getDirtyValues(this.group);
-      console.log(keyValue);
-
-      for (var key in keyValue) {
-        for (var keyInner in keyValue[key]) {
-          let temp: any = this.element.fields.values.filter(
-            (x: any) => keyInner == x.dependsOn
-          );
-          console.log(temp);
-
-          let dependStateArray: any;
-          if (temp[0] != null) {
-            dependStateArray = temp[0].dependState;
-          }
-
-          let original: any = keyValue[key];
-          if (dependStateArray != null) {
-            dependStateArray.forEach((el: any) => {
-              if (original[keyInner] === el) {
-                temp[0].hidden = false;
-                this.hidden = false;
-              }
-            });
-          }
-        }
-      }
-    });
-  }
-
-  getDirtyValues(form: any) {
-    let dirtyValues: any = {};
-
-    Object.keys(form.controls).forEach((key) => {
-      const currentControl = form.controls[key];
-
-      if (currentControl.dirty) {
-        if (currentControl.controls)
-          dirtyValues[key] = this.getDirtyValues(currentControl);
-        else dirtyValues[key] = currentControl.value;
-      }
-    });
-
-    return dirtyValues;
-  }
-
+export class NestedFormComponent { 
   @Input() element: any;
   @Input() formName: any;
-  @Input()
-  group!: UntypedFormGroup;
-  get addressArray(): FormArray {
-    return <FormArray>this.group.get(this.formName);
+  @Input()  group!: UntypedFormGroup; 
+
+  canShow(config: any) {
+    if (!config.dependsOn) {
+      return true;
+    } 
+    return config.dependState.includes(this.group.get(this.formName)?.get(config.dependsOn)?.value)
   }
+
 }
